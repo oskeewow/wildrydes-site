29379476fb7cb5af0852091d278347c349377301
oskeewow <58437620+oskeewow@users.noreply.github.com>
2024-12-15 12:01:37 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_3TYlsTGw1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '63sjfplq0pl1gk1mf2s7q9c5kt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_STTboxkKz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '19ftikm56gtg0rqodo6rkpe5mt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
